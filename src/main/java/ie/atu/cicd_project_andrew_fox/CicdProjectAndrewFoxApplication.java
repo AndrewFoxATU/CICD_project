7a4266efd5ad57f70caec49284a752fc2fe17089
AndrewFoxATU <G00417373@atu.ie>
2024-11-20 15:41:56 +0000

    first commit

---
+++ src/main/java/ie/atu/cicd_project_andrew_fox/CicdProjectAndrewFoxApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd_project_andrew_fox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdProjectAndrewFoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdProjectAndrewFoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/cicd_project_andrew_fox/CicdProjectAndrewFoxApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd_project_andrew_fox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdProjectAndrewFoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
